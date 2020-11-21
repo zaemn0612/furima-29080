632825633900095b79a4fd12d5252fa322344b8a
zaemn0612 <shinnishi0612@gmail.com>
2020-11-21 22:09:10 +0900

    バリデーションの設定
    空欄のまま登録すると保存できないように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday, presence: true       
 end
