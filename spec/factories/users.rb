c944e0379b428777b8752a778ca57998ec46980b
zaemn0612 <shinnishi0612@gmail.com>
2020-11-22 06:56:20 +0900

    FactoryBotの作成／テストコード用ファイル作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {"abcd00"}
+    password_confirmation {password}
+    family_name           {"山田"}
+    first_name            {"太郎"}
+    family_name_kana      {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birthday              {"1994-06-12"}
+  end
+end
