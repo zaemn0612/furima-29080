8ec97c487d29bb8e84a149a1dc5abcc3fd9218bf
zaemn0612 <shinnishi0612@gmail.com>
2020-11-22 22:32:22 +0900

    バリデーションの修正
    正規表現を参考に作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday, presence: true       
+  validates :nickname,presence: true 
+  validates :family_name,presence: true
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :first_name,presence: true
+            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :family_name_kana,presence: true
+            format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_kana,presence: true
+            format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :birthday,presence: true 
+  validates :password,presence: true 
+            format:{ with: ^([a-zA-Z0-9]{6,})$, message: "6文字以上の半角英数字"}
 end
