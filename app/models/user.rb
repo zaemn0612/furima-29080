674dd661f1a5b3da24c4f222cc55559d3f7a936b
zaemn0612 <shinnishi0612@gmail.com>
2020-11-23 00:05:15 +0900

    バリデーションの修正の修正②及びテストコードの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,presence: true 
-  validates :family_name,presence: true
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :first_name,presence: true
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :family_name_kana,presence: true
-            format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :first_name_kana,presence: true
-            format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :birthday,presence: true 
-  validates :password,presence: true 
-            format:{ with: ^([a-zA-Z0-9]{6,})$, message: "6文字以上の半角英数字"}
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :birthday, presence: true
 end
