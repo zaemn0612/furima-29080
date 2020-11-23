c62522a99f69d73ef69658243ecf6794323f4afd
zaemn0612 <shinnishi0612@gmail.com>
2020-11-23 16:21:55 +0900

    アソシエーションの追加
    ・itemモデル、userモデルそれぞれにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  
+  has_many:items
+
  with_options presence: true do
   validates :nickname,presence: true 
   validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
