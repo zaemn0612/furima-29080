ec7c7590d6faede3ff2194076ad7d9311ff95d7a
zaemn0612 <shinnishi0612@gmail.com>
2020-11-24 22:25:59 +0900

    コントローラーの設定
    ①new,createアクションを設定
    ②ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,18 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :price, :comment, :category_id, :product_condition_id, :shipping_charges_id, :shipping_address_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
+  end
+
 end
