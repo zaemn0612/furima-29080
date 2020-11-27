2de8fff9046f0f3273aef736045f1323ed1ecaa5
zaemn0612 <shinnishi0612@gmail.com>
2020-11-27 14:26:08 +0900

    ActiveHashに対するバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,8 @@ class Item < ApplicationRecord
   belongs_to :shipping_charges
   belongs_to :shipping_address
   belongs_to :estimated_shipping_date
+
+  validates :title, :text, presence: true
+  validates :category, :product_condition, :shipping_charges, :shipping_address, :estimated_shipping_date, numericality: { other_than: 1 } 
+
 end
