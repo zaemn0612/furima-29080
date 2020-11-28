86e48cb3eba20537c3df51a5fe7f7708f3306f61
zaemn0612 <shinnishi0612@gmail.com>
2020-11-28 10:43:37 +0900

    バリデーションの再設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_address
   belongs_to :estimated_shipping_date
 
+with_options presence: true do
   validates :title, :text, presence: true
-  validates :category, :product_condition, :shipping_charges, :shipping_address, :estimated_shipping_date, numericality: { other_than: 1 } 
-
+  validates :product_name, :comment, :category, :product_condition, :shipping_charges, :shipping_address, :estimated_shipping_date, numericality: { other_than: 1 } 
+  validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: "半角英数字"}
+  validates :price, format: { with: /[300-9999999]/, message: ""}
+ end
 end
