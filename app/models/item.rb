f051da57888508f94278b4d56e2b3fd81ede248b
zaemn0612 <shinnishi0612@gmail.com>
2020-11-30 22:46:14 +0900

    JSの設定／エラーの解除
    ・メインはJSの設定
    ・エラーの解除は下記
    ①アソシエーションの正しい記述
    ②バリデーションの正しい記載方法
    ③newページからのform_withの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,17 @@
 class Item < ApplicationRecord
   belongs_to:user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :product_condition
-  belongs_to :shipping_charges
+  belongs_to :shipping_charge
   belongs_to :shipping_address
   belongs_to :estimated_shipping_date
 
 with_options presence: true do
-  validates :title, :text, presence: true
-  validates :product_name, :comment, :category, :product_condition, :shipping_charges, :shipping_address, :estimated_shipping_date, numericality: { other_than: 1 } 
+  validates :product_name, :comment
+  validates :category_id, :product_condition_id, :shipping_charges_id, :shipping_address_id, :estimated_shipping_date_id, numericality: { other_than: 1 } 
   validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: "半角英数字"}
-  validates :price, format: { with: /[300-9999999]/, message: ""}
+  validates :price, format: { with: greater_than_or_equal_to: 300,less_than_or_equal_to: 9_999_999 message: ""}
  end
 end
