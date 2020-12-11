4ccba952418a288e0a8e285b89842c6ba6b8f4b2
zaemn0612 <shinnishi0612@gmail.com>
2020-12-11 19:39:58 +0900

    コントローラーの修正
    ・サーバーサイドに対して、出品者のみしか削除出来ないコードを実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if user_signed_in? && current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
+    end
   end
 
   private
