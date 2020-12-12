7a2551f6ade65edb53d41a177eeaacdc190104af
zaemn0612 <shinnishi0612@gmail.com>
2020-12-12 18:17:17 +0900

    コントローラー再修正①

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,22 @@
 class OrdersController < ApplicationController
   
   def index
-    # @user_order = UserOrder.all    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
+    @user_order = UserOrder.new    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
   end
 
   def create
     @user_order = UserOrder.new(order_params)
+    # binding.pry
     if @user_order.valid?
       @user_order.save
       redirect_to root_path
     else
-      render action: :index
+      render :index
     end 
   end
 
   private
-
+  
   def order_params
     params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number)
   end
