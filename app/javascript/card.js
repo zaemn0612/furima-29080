55c2f28f27d2eac9a86ae061cb4244a38c954f0f
zaemn0612 <shinnishi0612@gmail.com>
2020-12-26 15:17:34 +0900

    決済機能のクライアントサイド実装（サーバーサイドへのトークン送付）
    ・それに伴いviewの一部修正

---
+++ app/javascript/card.js
@@ -0,0 +1,36 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_72b4148a2de977b81e2f942e"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("user_order[number]"),
+      cvc: formData.get("user_order[cvc]"),
+      exp_month: formData.get("user_order[exp_month]"),
+      exp_year: `20${formData.get("user_order[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("user_order_number").removeAttribute("name");
+      document.getElementById("user_order_cvc").removeAttribute("name");
+      document.getElementById("user_order_exp_month").removeAttribute("name");
+      document.getElementById("user_order_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+      
+    });
+  });
+};
+
+window.addEventListener("load", pay);
