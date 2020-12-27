af555ce22c41cfdfa799514c392694c4f1e8c3f0
zaemn0612 <shinnishi0612@gmail.com>
2020-12-27 10:28:17 +0900

    単体テストコード

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -5,9 +5,10 @@ FactoryBot.define do
     municipality            {"浜松市"}
     address                 {"小森町１−１"}
     phone_number            {123456789}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
 
 # バリデーションを記載しているモデルに対しての単体コードであるため、自身で本ファイルを作成する（rails gなど使用せず）
-#　ターミナルの確認は FactoryBot.build(:user_order)を使用する。createでは完璧な状態が必要であるが、アソシエーション化が図れていないなどあるためbuildを使用
+#ターミナルの確認は FactoryBot.build(:user_order)を使用する。createは保存であり、完璧な状態が必要であるが、アソシエーション化が図れていないなどあるためbuildを使用
 # @user_order = FactoryBot(:user_order,item_id: 2, user_id: 1)
